57982e0c41bf483e55d5302d252aeb5e8a9374ee
John Cable <johncable@talk21.com>
2017-10-27 15:56:56 +0100

    Minor updates to Worklink scripts

--- SQL/CreateWorklinkDataTable.sql
+++ SQL/CreateWorklinkDataTable.sql
@@ -3,17 +3,29 @@ CREATE TABLE WORKLINKDATA (
     StudentID NUMBER(10),
     StudentIDModDate DATE,
     StudentExpiryDate DATE,
-    StudentIDExpiryModDate DATE,
-    StudentForename VARCHAR2(255),
-    StudentSurname VARCHAR2(255),
+    StudentExpiryModDate DATE,
+    StudentTitle VARCHAR2(100),
+    StudentForename VARCHAR2(100),
+    StudentSurname VARCHAR2(100),
+    StudentGender VARCHAR2(50),
+    StudentAddress1 VARCHAR2(100),
+    StudentAddress2 VARCHAR2(100),
+    StudentAddress3 VARCHAR2(100),
+    StudentTown VARCHAR2(100),
+    StudentCounty VARCHAR2(100),
+    StudentCountry VARCHAR2(100),
+    StudentPostCode VARCHAR2(50),
+    StudentTelephone VARCHAR2(255),
+    StudentEmail VARCHAR2(255),
+    StudentEthnicOrigin VARCHAR2(100),
     StudentDOB DATE,
-    StudentNINO VARCHAR2(255),
-    Mod_Date DATE,
+    StudentNINO VARCHAR2(50),
+    StudentModDate DATE,
     AltaPersonCode NUMBER(10),
     AltaForename VARCHAR2(30),
     AltaSurname VARCHAR2(30),
     AltaDOB DATE,
-    AltaNINUmber VARCHAR2(10)
+    AltaNINumber VARCHAR2(10) 
 );
 
-ALTER TABLE WORKLINKDATA ADD (AltaNINumber VARCHAR2(10));
+desc worklinkdata
--- SQL/Worklink.sql
+++ SQL/Worklink.sql
@@ -3,28 +3,37 @@
 desc WorklinkData
 SELECT COUNT(*) FROM WORKLINKDATA;
 
+-- Import data from external load file
+@e:\inetpub\wwwroot\UoB\SQL\InsertCommand.sql
+
 -- Alta matching process function definition
 @e:\inetpub\wwwroot\UoB\SQL\RunMatchWorklink.sql
 @e:\inetpub\wwwroot\UoB\SQL\MatchWorklink.sql
 @e:\inetpub\wwwroot\UoB\SQL\AnalyseWorklink.sql
 commit;
 
+-- Run Alta matching processes
 set serveroutput on
 exec RunMatchWorklink
 
---  Quick SQL checks
+--  Quick SQL checks (to validate the above analyses)
 select count(*) from worklinkdata where altapersoncode is null or altapersoncode < 1;
 select count(*) from worklinkdata where altaPersonCode is not null and ALTAPERSONCODE > 0 AND StudentForename <> AltaForename;
 select count(*) from worklinkdata where altaPersonCode is not null and ALTAPERSONCODE > 0 and StudentSurname <> AltaSurname;
 select count(*) from worklinkdata where altaPersonCode is not null and ALTAPERSONCODE > 0 AND TRUNC(StudentDOB) <> TRUNC(AltaDOB);
 select count(*) from worklinkdata where altaPersonCode is not null and ALTAPERSONCODE > 0 and StudentNINO <> AltaNINumber;
 
-
-select * from WORKLINKDATA;
-drop function MatchWorklink;
-DESC HES_PEOPLE
-
-select min(person_code) from hes_people;
-
-SELECT * FROM WORKLINKDATA;
+set serveroutput on
+delete from worklinkdata;
+commit;
+INSERT INTO WORKLINKDATA (studentid,studentidmoddate, studentexpirydate,studentidexpirymoddate,StudentTitle,StudentForename,
+StudentSurname,StudentGender, StudentAddress1, StudentAddress2, StudentAddress3, StudentTown, StudentCounty, StudentCountry,  
+StudentPostCode, StudentTelephone, StudentEmail, StudentEthnicOrigin,  StudentDOB,StudentNINO,Mod_Date,
+AltaPersonCode,AltaForename,AltaSurname,AltaDOb)                                                          
+VALUES ( 1508911 , to_date( '2016-03-31','YYYY-MM-DD'), to_date( '2016-03-23','YYYY-MM-DD'), to_date( '2016-03-31','YYYY-MM-DD'), 'Mr' , 'Zhirong' , 
+'Liang' , 'Male' , 'Room G47G, Future Engines and Fuels Lab, Mech Eng' , 'University of Brimingham' , '' , 'Edgbaston' , 'Birmingham' ,  , 
+'B152TT' , '' , 'ZXL411@bham.ac.uk' , 'Chinese' , to_date( '1988-12-09','YYYY-MM-DD'), 'TN091288M' , to_date( '2016-07-31','YYYY-MM-DD'), 
+0,' ',' ',to_date( '1900-01-01','YYYY-MM-DD'));
+commit;
+select * from worklinkdata;
 
