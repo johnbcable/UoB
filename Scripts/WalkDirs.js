5f9a0b6830d37e1d2599bcf0fa78eceec5298ee8
John Cable <johnbcable@me.com>
2017-11-03 19:18:21 +0000

    Added WalkDirs.js

---
+++ Scripts/WalkDirs.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var Path = require('path');
+
+// Now initialise the directory split character(s)
+// OS dependent:  \\\ for Windows; / for UNIX-like environments
+var splitter = '/';
+
+function WalkDirs(dirPath) {
+	console.log(dirPath);
+	fs.readdir(dirPath, function(err, entries) {
+		for (var idx in entries ) {
+			var fullPath = Path.join(dirPath, entries[idx]);
+			(function (fullPath) {
+				fs.stat(fullPath, function(err, stats) {
+					if (stats && stats.isFile()) {
+
+					    var fullparts = fullPath.split(splitter);
+
+					    // The next processing is dependent on where in that tree 
+					    // you want to start recording information from
+
+				    	var candidate = fullparts[2];
+    					var filedir = fullparts[3];
+    					var thefile = fullparts[4];
+    					var textline = candidate+",'"+filedir+"','"+thefile+"'\r\n";
+
+						fs.appendFileSync('WLdocs.csv', textline);
+
+						console.log(fullPath);
+					} else if (stats && stats.isDirectory()) {
+						WalkDirs(fullPath);
+					}
+				});
+			}) (fullPath);
+		}
+	});
+}
+WalkDirs("/etc");
