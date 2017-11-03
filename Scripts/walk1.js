da4c38d904df184d51af1ebe6dbcca41833c53e3
John Cable <johncable@talk21.com>
2017-11-03 15:39:24 +0000

    Updates to node scripts

--- Scripts/walk1.js
+++ Scripts/walk1.js
@@ -2,15 +2,26 @@ var fs = require('fs');
 var path = require('path');
 var walk = require('fs-walk');
 
-walk.walkSync('c:\\Work\\WorkLinkRawData\\WorkLinkFiles', function(basedir, filename, stat) {
+var processNode = function(basedir, filename, stat) {
     // var perm = stat.isDirectory() ? 0755 : 0644;
     var fullname = path.join(basedir, filename);
     var fullparts = fullname.split("\\");
 
     var curdir = fullparts[3];
     if (stat.isDirectory()) {
-    	walk.walkSync(fullname, )
+    	walk.walkSync(fullname,  processNode );
     } else {
-    	console.log(fullparts);
+    	// OK we have a file 
+    	// Get relevant details from it and out put to console
+    	var candidate = fullparts[6];
+    	var filedir = fullparts[7];
+    	var thefile = fullparts[8];
+    	var textline = candidate+",'"+filedir+"','"+thefile+"'\r\n";
+
+		fs.appendFileSync('WLdocs.csv', textline);
+
+    	// console.log(textline);
     }
-});
+};
+
+walk.walkSync('c:\\Work\\WorkLinkRawData\\WorkLinkFiles', processNode);
