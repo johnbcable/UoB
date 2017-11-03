4f5ef77ed0fd12e7ff6f64caa30656212e9815c5
John Cable <johncable@talk21.com>
2017-11-03 14:28:41 +0000

    Updates to walk1.js

--- Scripts/walk1.js
+++ Scripts/walk1.js
@@ -2,14 +2,15 @@ var fs = require('fs');
 var path = require('path');
 var walk = require('fs-walk');
 
-walk.walkSync('WorkLinkFiles', function(basedir, filename, stat) {
+walk.walkSync('c:\\Work\\WorkLinkRawData\\WorkLinkFiles', function(basedir, filename, stat) {
     // var perm = stat.isDirectory() ? 0755 : 0644;
     var fullname = path.join(basedir, filename);
     var fullparts = fullname.split("\\");
 
     var curdir = fullparts[3];
-
-    
-
-    console.log(fullparts);
+    if (stat.isDirectory()) {
+    	walk.walkSync(fullname, )
+    } else {
+    	console.log(fullparts);
+    }
 });
