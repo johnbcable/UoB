65d21726a43f11a9e0414c22bfc07bcd30e8fdf8
John Cable <johncable@talk21.com>
2017-11-06 09:19:33 +0000

    Added in analysis and SQL import of document info

---
+++ Scripts/WalkDirsSQL.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var Path = require('path');
+
+// Now initialise the directory split character(s)
+// OS dependent:  \\\ for Windows; / for UNIX-like environments
+var splitter = '\\';
+
+function WalkDirs(dirPath) {
+	console.log(dirPath);
+	fs.readdir(dirPath, function(err, entries) {
+		for (var idx in entries ) {
+			var fullPath = Path.join(dirPath, entries[idx]);
+			(function (fullPath) {
+				fs.stat(fullPath, function(err, stats) {
+					if (stats && stats.isFile()) {
+
+					    var fullparts = fullPath.split(splitter);
+
+					    // The next processing is dependent on where in that tree
+					    // you want to start recording information from
+
+				    	var candidate = fullparts[6];
+    					var filedir = fullparts[7];
+    					var thefile = fullparts[8];
+    					var textline = "INSERT INTO WORKLINKFILES(CANIDATEID,FOLDER,FILENAME) VALUES("+candidate+",'"+filedir+"','"+thefile+"');\r\n";
+
+						fs.appendFileSync('WLDOCS.SQL', textline);
+
+						console.log(fullPath);
+					} else if (stats && stats.isDirectory()) {
+						WalkDirs(fullPath);
+					}
+				});
+			}) (fullPath);
+		}
+	});
+}
+WalkDirs("c:\\Work\\WorkLinkRawData\\WorkLinkFiles");
