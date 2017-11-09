20466ceef97eb239080bb470f4417ba9c6b70f98
John Cable <johncable@talk21.com>
2017-11-09 15:38:58 +0000

    Updates tyo reorganised SQL code archive files

--- SQL/SQLServer/FileTypeQueries.sql
+++ SQL/SQLServer/FileTypeQueries.sql
@@ -1,9 +1,17 @@
-select * from  StoredFilePaths where RecordID in (14961);
-select * from StoredFileTypes;
-select fp.StoredFileTypeID, ft.FilePath, ft.Description, Count(*) 
-FROM StoredFilePaths fp, StoredFileTypes ft 
-WHERE fp.StoredFileTypeID = ft.StoredFileTypeID 
-GROUP BY fp.StoredFileTypeID, ft.FilePath, ft.Description 
+--
+--  Firstly run a query to generate the bulk of the analysis
+--  of file counts per document type
+--
+select fp.StoredFileTypeID, ft.FilePath, ft.Description, Count(*)
+FROM StoredFilePaths fp, StoredFileTypes ft
+WHERE fp.StoredFileTypeID = ft.StoredFileTypeID
+GROUP BY fp.StoredFileTypeID, ft.FilePath, ft.Description
 ORDER BY ft.FilePath, ft.Description;
-
- 
+--
+--  Information about CV's and AdminCV's is not found
+--  by the above query. Run the query below to get file
+--  totals for these types of documents.
+--
+select 'CVs', count(*) from dbo.VacancyApplications where CVID > 0
+union
+select 'Admin CVs', count(*) from dbo.VacancyApplications where AdminCVID > 0;
