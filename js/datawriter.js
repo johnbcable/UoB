8cf2f214a2c45c076beb3401c620e596d2ff37cf
John Cable <johncable@talk21.com>
2017-11-10 15:51:51 +0000

    updates to REST web pages to try and generate XML

--- js/datawriter.js
+++ js/datawriter.js
@@ -61,3 +61,80 @@
         link.setAttribute('download', filename);
         link.click();
     }
+
+    /**
+    *  VERSION // 0.1
+    *  AUTHOR // siegfried.ehret@gmail.com
+    *
+    *  LICENSE //
+    *
+    *  DO WTF YOU WANT TO PUBLIC LICENSE
+    *                   Version 2, December 2004
+    *
+    * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+    *
+    * Everyone is permitted to copy and distribute verbatim or modified
+    * copies of this license document, and changing it is allowed as long
+    * as the name is changed.
+    *
+    *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+    *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+    *
+    * 0. You just DO WHAT THE FUCK YOU WANT TO.
+    */
+    var json2xml = (function (my, undefined) {
+      "use strict";
+      var tag = function(name, options) {
+        var mynumber = parseInt(name);
+        var realtag = name;
+        if (! (isNaN(mynumber))) {
+          realtag = "item"+realtag;
+        }
+        options = options || {};
+        return "<"+(options.closing ? "/" : "")+realtag+">";
+      };
+      var exports = {
+        convert:function(obj, rootname) {
+          var xml = "";
+          for (var i in obj) {
+            if(obj.hasOwnProperty(i)){
+              var value = obj[i], type = typeof value;
+              if (value instanceof Array && type == 'object') {
+                for (var sub in value) {
+                  xml += exports.convert(value[sub]);
+                }
+              } else if (value instanceof Object && type == 'object') {
+                xml += tag(i)+exports.convert(value)+tag(i,{closing:1});
+              } else {
+                xml += tag(i)+value+tag(i,{closing:1});
+              }
+            }
+          }
+          return rootname ? tag(rootname) + xml + tag(rootname,{closing:1}) : xml;
+        }
+      };
+      return exports;
+    })(json2xml || {});
+
+    function downloadXML(args) {
+        var data, filename, link, sourcedata, docroot;
+        var dummyroot = 'root';
+
+        sourcedata = args.data || noData;
+        docroot = args.root || dummyroot;
+        var xml = json2xml.convert(sourcedata, docroot);
+
+        if (xml == null) return;
+
+        filename = args.filename || 'export.xml';
+
+        if (!xml.match(/^data:text\/xml/i)) {
+            xml = 'data:text/xml;charset=utf-8,' + xml;
+        }
+        data = encodeURI(xml);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    }
